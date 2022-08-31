fb7b4647a9533f5c4ca917c666b4ceae0b447a27
Nick <32347189+njnelso@users.noreply.github.com>
2022-08-31 16:39:45 -0400

    Add files via upload
    Python Replit Project #1
    Questions 1-3

---
+++ main.py
@@ -0,0 +1,57 @@
+# --------------------------------------------------------------------- #
+# Welcome to the main.py file. We will use this file for most of our work in REPLIT.
+
+# Note the hashtag (#) symbol at the front of a line indictes that it is a comment.
+
+# Comments are for humans, and will be skipped by the python interpreter.
+
+# As we progress through the Python section, we will learn new concepts and build upon them.
+
+# When you code in REPLIT, you will write your code in between comments that look like this:
+
+### YOUR CODE STARTS HERE ###
+
+### YOUR CODE ENDS HERE ###
+
+# When you are ready to test your code, click the checkmark in the left bar and click "Run tests". They are named for the corresponding problem.
+
+# Once you have passed all the tests, click the submit button in the upper right corner
+
+# IMPORTANT: DO NOT EDIT ANY OTHER CODE OR COMMENTS ON THIS PAGE
+
+# Time to get started... good luck and have fun!
+
+# ---------------------------------------------------------------------- #
+'''Q1. Hello World'''
+
+# Print out the string 'Hello World!'
+
+### YOUR CODE STARTS HERE ###
+
+print("Hello World!")
+
+### YOUR CODE ENDS HERE ###
+
+# ---------------------------------------------------------------------- #
+'''Q2. Create a Variable - (No test cases for this)'''
+
+# Create a variable called name, and assign it to the value of your first name.
+
+### YOUR CODE STARTS HERE ###
+
+name = "Nick"
+
+### YOUR CODE ENDS HERE ###
+
+# ---------------------------------------------------------------------- #
+'''Q3. Name String'''
+
+# Use concatenation, and the name variable you declared in problem 2 above to print out the phrase: 'My name is <NAME> and I will be a Python Master!'
+
+### YOUR CODE STARTS HERE ###
+
+print("My name is " + name + " and I will be a Python Master!")
+
+### YOUR CODE ENDS HERE ###
+
+# ---------------------------------------------------------------------- #
