6b7498232d2c9d67dd0d90949f0b154d328f303c
Nick <32347189+njnelso@users.noreply.github.com>
2022-09-01 12:48:41 -0400

    Add files via upload
    Just a few notes

---
+++ Aug 31 python notes.py
@@ -0,0 +1,45 @@
+# Review from yesterday
+# numbers (int)
+# words (strings)
+# operators (+, -, *, /, //, %, **)
+# print() - prints to console
+# type() - tells the datatype
+# type casting - str()
+# variables and variable names
+# Reviewing basing math:
+print(5 + 5)
+# We can also do math with variables:
+num1 = 100
+num2 = 25
+print(num1 + num2)
+print('Ryan' * num2)
+# same goes with string types
+str1 = 'banana'
+str2 = 'pancakes'
+print(str1 + ' ' + str2)
+print(str1 + str2)
+# lets chat a little about modulation
+# % gives the remainder ... we will often use it to determine even/odd (modulate by 2)
+a = 5
+b = 4
+c = 150 + 277 - 13 // 6
+print(c)
+print(c % 2)
+# Other datatypes we have not learned about yet
+# Boolean - True or False (one, off) (is, isn't)
+name = True
+print(type(name))
+# Truthy v Falsy
+# what is Truthy?
+# any number that is NOT 0
+# any string that contains at least one character (even a space)
+# what is Falsy?
+# 0 is Falsy
+# empty strings are Falsy '' (empty strings are NOT ' ')
+# lets chat about float point numbers (floats)
+# integers are whole numbers: 0, 100, -5, 1000
+# floats are any number in between these ints: 5.5, -0.234, 99.9
+print(int(25 / 5))
+my_num = 25 / 5
+print(int(my_num))
+print(25 // 5)
