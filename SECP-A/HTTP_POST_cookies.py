8edbdac239853a7d733805777917bb32aca21afb
Nick <32347189+njnelso@users.noreply.github.com>
2022-09-23 11:49:14 -0400

    Update and rename HTTP_POST_cookies.py to SECP-A/HTTP_POST_cookies.py

--- SECP-A/HTTP_POST_cookies.py
+++ SECP-A/HTTP_POST_cookies.py
@@ -2,8 +2,11 @@
 
 import requests
 
-url = 'http://shell.fullstackacademy.com:8861/'
-cookies = {'amount': '7'}
+url = '<Insert URL here>'
+cookies = {'name': 'value'}
 ### headers = {'Accept-Encoding': 'identity'} >> apparently not needed, thought i needed to specify that the output would be html
 r = requests.post(url, cookies=cookies)
 print(r.text)
+
+
+### the cURL request in bash accomplishes the same task >> curl -X POST -b "cookie=value" <~Insert URL~>/ <<
